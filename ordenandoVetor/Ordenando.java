bf29bc18897dc920f814715967d759f70407c493
JoaquimGCVS <jgui9292@gmail.com>
2025-03-15 18:27:50 -0300

    treinando ordenacao por selecao

---
+++ ordenandoVetor/Ordenando.java
@@ -0,0 +1,22 @@
+package ordenandoVetor;
+
+public class Ordenando {
+    public static void main (String args[]) {
+
+    }
+
+    static int[] ordenaVetor(int[] vetor) {
+        for (int referencia=0; referencia < vetor.length-1; referencia++) {
+            int posicaoMenorValor = referencia;
+            for (int posicaoNumeroComparado = referencia+1 ; posicaoNumeroComparado<vetor.length; posicaoNumeroComparado++) {
+                if(vetor[posicaoMenorValor]>vetor[posicaoNumeroComparado]) {
+                    posicaoMenorValor=posicaoNumeroComparado;
+                }
+            }
+            int varivelTemporaria = vetor[referencia];
+            vetor[referencia]=vetor[posicaoMenorValor];
+            vetor[posicaoMenorValor]=varivelTemporaria;
+        }
+        return vetor;
+    }
+}
