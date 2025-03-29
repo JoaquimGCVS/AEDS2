7cfeaee5625dce137bc3361c9cf3b50bb8c78250
JoaquimGCVS <jgui9292@gmail.com>
2025-03-29 14:04:02 -0300

    Estudano metodos de ordenacao de vetores

--- Ordenacao/InsertionSort.java
+++ Ordenacao/InsertionSort.java
@@ -24,7 +24,7 @@ public class InsertionSort {
         for (int i=1;i<array.length;i++) {
             int temp = array[i];
             int j=i-1;
-            while (j>=0 && temp<array[j]) {
+            while (j>=0 && array[j]>temp) {
                 array[j+1]=array[j];
                 j--;
             }
